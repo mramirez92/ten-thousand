5f3871508800d2780f3330380fe1ee1921eb5df7
Monica <ramirez.92.monica@gmail.com>
2022-12-27 17:12:49 -0600

    implement roll function

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -4,15 +4,7 @@ from random import randint
 class GameLogic:
 
     # kind of its own thing, static method has input and has outputs, just needs a place to live
-
+    # returns tuple of random integers, between 1 and 6
     @staticmethod
     def roll_dice(num_dice):
-        """
-        param num_dice
-        return tuple of interger values between 1 and 6
-        """
-        """
-        random_nums=[1] is a list
-        return tuple(random_nums) turns our list into a tupple
-        """
-        return 1,
+        return tuple(randint(1, 6) for _ in range(0, num_dice))
