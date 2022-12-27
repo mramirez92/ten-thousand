8a64e81264012bcb41bc79c78e09ac7a5697f07c
Monica <ramirez.92.monica@gmail.com>
2022-12-27 16:51:26 -0600

    first commit

---
+++ ten_thousand/game_logic.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+class GameLogic:
+
+    # kind of its own thing, static method has input and has outputs, just needs a place to live
+
+    @staticmethod
+    def roll_dice(num_dice):
+        """
+        param num_dice
+        return tuple of interger values between 1 and 6
+        """
+        """
+        random_nums=[1] is a list
+        return tuple(random_nums) turns our list into a tupple
+        """
+        return 1,
