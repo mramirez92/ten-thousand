bc3bd0193a8b364ded202b5f857763524b11b883
Monica <ramirez.92.monica@gmail.com>
2022-12-27 20:50:15 -0600

    add n of kind funcntions for 3 and 4

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,4 +1,5 @@
 from random import randint
+from collections import Counter
 
 
 class GameLogic:
@@ -8,3 +9,28 @@ class GameLogic:
     @staticmethod
     def roll_dice(num_dice):
         return tuple(randint(1, 6) for _ in range(0, num_dice))
+
+    @staticmethod
+    def calculate_score(roll):
+
+        n_of_kind = {1: 1000, 2: 200, 3: 300, 4: 400, 5: 500, 6: 600}
+
+        score = 0
+
+        # counter returns value pair ex ({5: 1})
+
+        counter_dice = Counter(roll)
+
+        for dice_num, counts in counter_dice.items():
+            if dice_num == 5 and counts <= 2:
+                score += 50 * counts
+            elif dice_num == 1 and counts <= 2:
+                score += 100 * counts
+            elif counts == 3:
+                score += n_of_kind[dice_num]
+            elif counts == 4:
+                score += n_of_kind[dice_num]*2
+            else:
+                score += 0
+
+        return score
